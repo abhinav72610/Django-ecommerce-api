31faf99ef852027a54a578542988b1885b231de8
Abhinav kumar <abhinav72610@gmail.com>
2021-01-19 23:06:17 +0530

    views and serializers added

---
+++ EcommerceAPI/djangoAPIapp/serializers.py
@@ -0,0 +1,47 @@
+
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        fields = (
+            'id',
+            'title'
+        )
+        model = Category 
+
+
+class BookSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username', read_only=False)
+    class Meta:
+        fields = (
+            'id',
+            'title',
+            'category',
+            'author',
+            'isbn',
+            'pages',
+            'price',
+            'stock',
+            'description',
+            'imageUrl',
+            'created_by',
+            'status',
+            'date_created'
+        )
+        model = Book
+
+class ProductSerializer(serializers.ModelSerializer):
+    created_by = serializers.ReadOnlyField(source='created_by.username', read_only=False)
+    class Meta:
+        fields = (
+            'id',
+            'product_tag',
+            'name',
+            'category',
+            'price',
+            'stock',
+            'imageUrl',
+            'created_by',
+            'status',
+            'date_created'
+        )
+        model = Product
